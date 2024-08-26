59063630dd7e134b54e2d1f73d2fb9ee9f22a76d
Thabo <thabochambule1@gmail.com>
2024-08-26 15:25:30 +0200

    add function for checking win

--- scripts/script.js
+++ scripts/script.js
@@ -59,6 +59,13 @@ const Game = (() => {
       return;
     }
     Gameboard.update(index, players[currentPlayerIndex].mark);
+
+    if(checkForWin(Gameboard.getGameboard,players[currentPlayerIndex].mark)){
+      gameOver = true;
+      alert(`${players[currentPlayerIndex].name}won!`)
+    }
+
+
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0; // Switch player
   };
   const restart = () => {
@@ -75,7 +82,29 @@ const Game = (() => {
   };
 })();
 
-const restartButton = document.querySelector("#start-button");
+function checkForWin(board){
+  const winningCombination = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  for(let i =0; i < winningCombination.length;i++){
+    const [a,b,c] = winningCombination[i];
+    if(board[a] === board[b] && board[a] === board[a]){
+      
+    }
+  }
+
+}
+
+
+
+const restartButton = document.querySelector("#restart-button");
 restartButton.addEventListener("click", ()=>{
 Game.restart();
 
